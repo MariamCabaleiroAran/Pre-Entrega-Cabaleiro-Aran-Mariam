2c6f3f3778dc8095b442ccfd6e44aeb9cc2e0ab8
Mariam Cabaleiro Arán <mariamcabaleiro@gmail.com>
2025-10-24 19:15:00 -0300

    enlaces activos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styles from './Navbar.module.css'; 
 
 const Nav = () => { 
     return (  
         <nav style={{ backgroundColor: "#ffffffff", color: "white", padding: "10px" }}>  
             <ul style={{ listStyle: "none", display: "flex", gap: "24px", padding: 0, margin: 0 }}>
-                <li><Link to={'/'}>PRODUCTOS</Link> </li> 
-                <li><Link to="/ultimosdias">ULTIMOS DIAS</Link></li> 
-                <li><Link to="/sugeridos">SUGERIDOS</Link>
+                <li><NavLink to={'/'}className={({ isActive }) => (isActive ? styles.active : styles.link)}>PRODUCTOS</NavLink> </li> 
+                <li><NavLink to="/ultimosdias"className={({ isActive }) => (isActive ? styles.active : styles.link)}>ULTIMOS DIAS</NavLink></li> 
+                <li><NavLink to="/sugeridos"className={({ isActive }) => (isActive ? styles.active : styles.link)}>SUGERIDOS</NavLink>
                 </li>   
             </ul>  
         </nav>  
